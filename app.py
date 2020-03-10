5c17830460158d49b04e79e342b64a20e4eb5e5b
Kat Jansen <kat@17cosmicln.net>
2020-03-10 18:14:00 -0500

    Replace flask with fastapi. Import session and engine from db.py. Create 2 api routes, 1 per table. Not configured for our db or tables.

--- app.py
+++ app.py
@@ -1,56 +1,73 @@
-from flask import Flask, render_template, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from db import SessionLocal, engine
+
 import datetime as dt
 import numpy as np
 import random
 
+models.Base.metadata.create_all(bind=engine)
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Resources/hawaii.sqlite"
-db = SQLAlchemy(app)
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.route("/")
-def home():
+# Dependency
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+async def home():
     # list of links to other routes
-    return render_template("index.html")
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
-@app.route("/random")
+@app.get("/random")
 def generate_scooter():
     # generate random # - display scooter ride
     return render_template("index.html")
 
-@app.route("/difference")
+@app.get("/difference")
 def zipcode_vs_census():
     # 2 addresses same zipcode, diff census track, how the data differs
     return render_template("index.html")
 
-@app.route("/<zipcode>")
+@app.get("/{zipcode}")
 def zip_stats():
     # % compaint, # rides in,m # rides out, max expense, min expense, avg, complaints, maps and other stats
     return render_template("index.html")
 
-@app.route("/long")
+@app.get("/long")
 def longest():
     # display longest ride distance/time
     return render_template("index.html")
 
-@app.route("/nowhere")
+@app.get("/nowhere")
 def nowhere():
     # rides that go nowhere, some plots on this. where/when
     return render_template("index.html")
 
-@app.route("/red_zone")
+@app.get("/red_zone")
 def red_zone():
     #  red zone zip code (10 worst neighbor hoods to leave scooter in)
     return render_template("index.html")
 
-@app.route("/api/")
-def api():
-    # return json api based on parameters from url
-    return render_template("index.html")
 
-@app.route("/api_docs")
+#API Routes
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+@app.get("/api_docs")
 def api_docs():
     # Documentation for API
     return render_template("index.html")
