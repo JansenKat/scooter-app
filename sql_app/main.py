629553819156b256776c065acdbcbad5ada16ebe
Kat Jansen <kat@17cosmicln.net>
2020-03-18 19:39:24 -0500

    Comment out other routes. They're breaking the api routes.

--- sql_app/main.py
+++ sql_app/main.py
@@ -43,35 +43,35 @@ async def home(request: Request):
     # list of links to other routes
     return templates.TemplateResponse("index.html",{"request": request})
 
-@app.get("/random")
-def generate_scooter():
-    # generate random # - display scooter ride
-    return templates.TemplateResponse("index.html",{"request": request})
+# @app.get("/random")
+# def generate_scooter(request: Request):
+#     # generate random # - display scooter ride
+#     return templates.TemplateResponse("index.html",{"request": request})
 
-@app.get("/difference")
-def zipcode_vs_census():
-    # 2 addresses same zipcode, diff census track, how the data differs
-    return templates.TemplateResponse("index.html",{"request": request})
+# @app.get("/difference")
+# def zipcode_vs_census(request: Request):
+#     # 2 addresses same zipcode, diff census track, how the data differs
+#     return templates.TemplateResponse("index.html",{"request": request})
 
-@app.get("/{zipcode}")
-def zip_stats():
-    # % compaint, # rides in,m # rides out, max expense, min expense, avg, complaints, maps and other stats
-    return templates.TemplateResponse("index.html",{"request": request})
+# @app.get("/{zipcode}")
+# def zip_stats(zipcode, request: Request):
+#     # % compaint, # rides in,m # rides out, max expense, min expense, avg, complaints, maps and other stats
+#     return templates.TemplateResponse("index.html",{"request": request})
 
-@app.get("/long")
-def longest():
-    # display longest ride distance/time
-    return templates.TemplateResponse("index.html",{"request": request})
+# @app.get("/long")
+# def longest(request: Request):
+#     # display longest ride distance/time
+#     return templates.TemplateResponse("index.html",{"request": request})
 
-@app.get("/nowhere")
-def nowhere():
-    # rides that go nowhere, some plots on this. where/when
-    return templates.TemplateResponse("index.html",{"request": request})
+# @app.get("/nowhere")
+# def nowhere(request: Request):
+#     # rides that go nowhere, some plots on this. where/when
+#     return templates.TemplateResponse("index.html",{"request": request})
 
-@app.get("/red_zone")
-def red_zone():
-    #  red zone zip code (10 worst neighbor hoods to leave scooter in)
-    return templates.TemplateResponse("index.html",{"request": request})
+# @app.get("/red_zone")
+# def red_zone(request: Request):
+#     #  red zone zip code (10 worst neighbor hoods to leave scooter in)
+#     return templates.TemplateResponse("index.html",{"request": request})
 
 
 #API Routes
@@ -85,5 +85,5 @@ def read_trips(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     trips = crud.get_trips(db, skip=skip, limit=limit)
     return trips
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
