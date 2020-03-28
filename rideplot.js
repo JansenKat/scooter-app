4453ca6d76ead823c2c14ea91e3ccb872a5bb414
Sana Khan <sana.khan@sailpoint.com>
2020-03-27 20:30:18 -0500

    change background for plot

--- rideplot.js
+++ rideplot.js
@@ -5,7 +5,7 @@ Plotly.d3.csv('final_copy.csv', function(err, rows){
     function getMaxOfArray(numArray) {
         return Math.max.apply(null, numArray);
     }
-
+    var scl =['rgb(213,62,79)','rgb(244,109,67)','rgb(253,174,97)','rgb(254,224,139)','rgb(255,255,191)','rgb(230,245,152)','rgb(171,221,164)','rgb(102,194,165)','rgb(50,136,189)','rgb(60,176,129)'];
     var data = [];
     var count = unpack(rows, 'cnt');
     console.log(count)
@@ -17,8 +17,8 @@ Plotly.d3.csv('final_copy.csv', function(err, rows){
     console.log("ending cordinates" + endLat);
     console.log("starting cordinates" + startLat);
     console.log("start long" + startLongitude);
-  
     console.log("end long" + endLongitude);
+
     for ( var i = 0 ; i < count.length; i++ ) {
         var opacityValue = count[i]/getMaxOfArray(count);
 
@@ -30,8 +30,8 @@ Plotly.d3.csv('final_copy.csv', function(err, rows){
             lat: [ startLat[i] , endLat[i]],
             mode: 'lines+markers',
             line: {
-                width: 10,
-                color: 'red'
+                width: 2,
+                color: scl[i]
             },
             opacity: opacityValue
         }
@@ -60,21 +60,27 @@ Plotly.d3.csv('final_copy.csv', function(err, rows){
   var layout = {
     dragmode: "zoom",
     mapbox: {
-      style: "white-bg",
-      layers: [
-        {
-          sourcetype: "raster",
-          source: ["https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"],
-          below: "traces"
-        }
-      ],
+      style: "dark",
+      // // layers: [
+      //   {
+      //     sourcetype: "raster",
+      //     source: ["https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"],
+      //     below: "traces"
+      //   }
+      // ],
       center: { lat: 30.2280314, lon: -97.7734004 },
       zoom: 10
     },
     margin: { r: 0, t: 0, b: 0, l: 0 }
   };
 
-    Plotly.newPlot("myDiv", data, layout, {showLink: false});
+  Plotly.setPlotConfig({
+    mapboxAccessToken:"pk.eyJ1Ijoic2toYW4wNyIsImEiOiJjazg4dXNsNmUwMGFuM2ZudHNiaXU1Y3kwIn0.uDCrOvOHUNL7qdsiOUwMPA"
+  });
+
+  Plotly.newPlot('myDiv', data, layout, {displayModeBar: false})
+
+  ;
 
 });
 
