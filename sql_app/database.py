73c3390551632f7dc2c522dfe00405775f055ef5
Kat Jansen <kat@17cosmicln.net>
2020-04-04 15:25:08 -0500

    Edit connection string in db file.

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@127.0.0.1:3306/proj2db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL#, connect_args={"check_same_thread": False}
