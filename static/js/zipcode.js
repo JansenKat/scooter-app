469391ef91114ccee00e3eefb52baa73c08c3d6f
Thomas Gomez <thomasg@indeed.com>
2020-04-05 01:36:19 -0500

    HTML Page improvements. Footer work. Zipcode Table.

--- static/js/zipcode.js
+++ static/js/zipcode.js
@@ -1,55 +1,55 @@
-function buildTable(sample) {
+Plotly.d3.json("/zipcode_api", function (err, rows) {
 
-    let s = d3.json(`/zipcode/${sample}`).then((data) => {
+    function unpack(rows, key) {
+        return rows.map(function (row) { return row[key]; });
+    }
 
-        //Make Table
-        var tableData = [{
-            type: 'table',
-            header: {
-                values: headerValues,
-                align: "center",
-                line: { width: 1, color: 'rgb(50, 50, 50)' },
-                fill: { color: ['rgb(0,191,255)'] },
-                font: { family: "Helvetica Neue", size: 12, color: "white" }
+    var headerNames = Plotly.d3.keys(rows[0]);
+    var rowEvenColor = "#e5e4e2";
+    var rowOddColor = "#f8f8ff";
+
+    var headerValues = [];
+    var cellValues = [];
+    for (i = 0; i < headerNames.length; i++) {
+        headerValue = [headerNames[i]];
+        headerValues[i] = headerValue;
+        cellValue = unpack(rows, headerNames[i]);
+        cellValues[i] = cellValue;
+    }
+
+    var data = [{
+        type: 'table',
+        // columnwidth: [500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500],
+        // columnorder: [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12],
+        header: {
+            values: headerValues,
+            align: "center",
+            line: { width: 1, color: 'rgb(50, 50, 50)' },
+            fill: { color: ['#007bff'] },
+            font: { family: "Helvetica Neue", size: 12, color: "white" }
+        },
+        cells: {
+            values: cellValues,
+            align: ["center", "center"],
+            line: { color: "black", width: 1 },
+            fill: {
+                color: [[rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor,
+                    rowEvenColor, rowOddColor, rowEvenColor, rowOddColor, rowEvenColor, rowOddColor, rowEvenColor]]
             },
-            cells: {
-                values: cellValues,
-                align: ["center", "center"],
-                line: { color: "black", width: 1 },
-                fill: { color: ['rgb(240,248,255)'] },
-                font: { family: "Helvetica Neue", size: 12, color: ["black"] }
-            }
-        }]
+            font: { family: "Helvetica Neue", size: 12, color: ["black"] }
+        }
+    }]
 
-        Plotly.newPlot("zipcodeDiv", tableData)
+    var layout = {
+        title: "Zipcode Analytics",
+        showlegend: false
+    }
 
-    })
-}
-
-function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-
-    // Use the list of sample names to populate the select options
-    d3.json("/zipcode_api").then((sampleNames) => {
-        sampleNames.forEach((sample) => {
-            selector
-                .append("option")
-                .text(sample)
-                .property("value", sample);
-        });
-
-        // Use the first sample from the list to build the initial plots
-        const firstSample = sampleNames[0];
-        buildTable(firstSample);
-    });
-}
-
-function optionChanged(newSample) {
-    // Fetch new data each time a new sample is selected
-    console.log(`option changed: ${newSample} selected`)
-    buildTable(newSample);
-}
-
-// Initialize the dashboard
-init();
+    Plotly.newPlot('zipcodeDiv', data, layout, { displayModeBar: true, modeBarButtonsToRemove: ['closest'], displaylogo: false });
+});
