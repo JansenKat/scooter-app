a7f69695b93f87cbc2f2a2e10bc65e42af4fe9e1
Sana Khan <sana.khan@sailpoint.com>
2020-04-06 17:44:12 -0500

    update file to remove comments

--- static/js/redzone.js
+++ static/js/redzone.js
@@ -1,4 +1,3 @@
-// function buildCharts() {
 
 Plotly.d3.json('/redzone_api', function(data) {
             console.log(data)
@@ -36,31 +35,4 @@ Plotly.d3.json('/redzone_api', function(data) {
     }
 })
 
-// var xValue = ['Product A', 'Product B', 'Product C'];
 
-// var yValue = [20, 14, 23];
-// var trace1 = {
-//     x: xValue,
-//     y: yValue,
-//     type: 'bar',
-//     text: yValue.map(String),
-//     textposition: 'auto',
-//     hoverinfo: 'none',
-//     marker: {
-//       color: 'rgb(158,202,225)',
-//       opacity: 0.6,
-//       line: {
-//         color: 'rgb(8,48,107)',
-//         width: 1.5
-//       }
-//     }
-//   };
-  
-//   var data = [trace1];
-  
-//   var layout = {
-//     title: 'January 2013 Sales Report',
-//     barmode: 'stack'
-//   };
-  
-//   Plotly.newPlot('red-zoneDiv2', data, layout);
